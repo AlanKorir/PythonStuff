fc1fe755d8f1ed9a333fbf02da1cfe6e5a9b947b
Alan Korir <alan.korir@dimensiondata.com>
2023-07-05 23:07:10 +0300

    b

--- pyclasses.py
+++ pyclasses.py
@@ -1,4 +1,8 @@
 class Person:
+    numOfPersons = 0
+
+    raiseAmount = 1.04 
+
     def __init__(self, firstName, lastName, salary):
         # self.age = age
         # self.weight = weight
@@ -7,15 +11,21 @@ class Person:
         self.lastName = lastName
         self.salary = salary
         # self.catch = catch
+        
+        Person.numOfPersons += 1
 
     def fullname(self):
         return '{} {}'.format(self.firstName, self.lastName)
 
     def employeeRaise(self):
-        self.salary = int(self.salary * 1.4)
+        self.salary = int(self.salary * self.raiseAmount)
 
 user1 =  Person("John", "Doe", 300000)
+user2 = Person("Jane", "Doe", 40000)
+Person.raiseAmount = 2.06
 user1.employeeRaise()
-print(user1.salary)
 
+print(user1.salary)
+print(Person.raiseAmount)
+print (Person.numOfPersons)
 
